3e5c70cbd99f41e9f0115088964213d4dcee4d89
Gael-iceman <rutayisiregael2000@gmail.com>
2024-03-12 03:27:06 +0200

    first commit

---
+++ gael.js
@@ -0,0 +1,10 @@
+module.exports.add = function add(num1,num2){
+    return num1+num2;
+}
+module.exports.sub =function sub(num1,num2){
+    return num1-num2;
+}
+
+module.exports.multiply = function multiply(num1,num2){
+    return num1*num2;
+}
